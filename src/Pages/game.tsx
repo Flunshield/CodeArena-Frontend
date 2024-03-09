93f3a10e3cf505aa9f14faab45ef539e5e43e133
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-03-09 17:07:14 +0100

    feat: ajout btn envoie test + card test passing

--- src/Pages/game.tsx
+++ src/Pages/game.tsx
@@ -8,6 +8,7 @@ import CardContent from "../ComposantsCommun/CardContent.tsx";
 import MyTimer from "../ComposantsCommun/myTimer.tsx";
 import {postTest} from "../Helpers/apiHelper.ts";
 import {responseTest} from "../Interface/Interface.ts";
+import Button from "../ComposantsCommun/Button.tsx";
 
 
 const Game = () => {
@@ -45,21 +46,24 @@ const Game = () => {
     };
 
     const time = new Date();
-    time.setSeconds(time.getSeconds() + 5); // 10 minutes timer
+    time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
 
+    const handleSendTest = () => {
+        postTest('testsJs', {code: code, tests: testsAjouer}).then(async (response) => {
+            const result: responseTest = await response.json();
+            if (result) {
+                setTestPassed(result.testPassed);
+                setTestFailed(result.testFailed);
+                setSuccess(result.success);
+            } else {
+                console.log('Test failed');
+            }
+        });
+    }
     useEffect(() => {
         if(remainingTime === 0){
             setIsDisabled(true);
-            postTest('testsJs', {code: code, tests: testsAjouer}).then(async (response) => {
-                const result: responseTest = await response.json();
-                if (result) {
-                    setTestPassed(result.testPassed);
-                    setTestFailed(result.testFailed);
-                    setSuccess(result.success);
-                } else {
-                    console.log('Test failed');
-                }
-            });
+            handleSendTest();
         }
     }, [remainingTime]);
 
@@ -88,6 +92,32 @@ const Game = () => {
                             <h2 className="text-3xl font-bold">Sujet</h2>
                             <p className="mt-5">Créer une fonction permettant d&lsquo;additionner.</p>
                         </CardContent>
+                        <CardContent className="flex justify-center flex-col p-0 text-white border-2 rounded-md">
+                            <h2 className="text-3xl font-bold">Etat des Tests</h2>
+                            <ul>
+                                <li>
+                                    <p>Tests passés {testPassed.length} / {testsAjouer.length}</p>
+                                    <ul>
+                                        {testPassed.map((test) => (
+                                            <li key={test} className="">
+                                                <p className="font-bold text-green-600">{test}</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                                <li className="mt-5">
+                                    <p>Tests ratés {testFailed.length} / {testsAjouer.length}</p>
+                                    <ul>
+                                        {testFailed.map((test) => (
+                                            <li key={test} className="">
+                                                <p className="font-bold text-red">{test}</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            </ul>
+                        </CardContent>
+                        <Button id="btn-send-test" type="button" className="border-2 rounded-md mt-5 p-2 w-auto text-white bg-green-800" onClick={handleSendTest}>Teste ton code</Button>
                     </Card>
                 </div>
             </div>
