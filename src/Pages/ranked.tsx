d8c611f26c4314a5c2a40bf05ff923956b378276
Kévin Begot <begot.kevin@gmail.com>
2024-03-21 10:51:05 +0100

    (add: rankd page)

---
+++ src/Pages/ranked.tsx
@@ -0,0 +1,15 @@
+import Layout from "../ComposantsCommun/Layout.tsx";
+
+function Ranked() {
+
+    return (
+        <Layout>
+            <div className="m-32 h-full bg-secondary rounded-lg text-white">
+
+                salut
+            </div>
+        </Layout>
+    );
+}
+
+export default Ranked;
