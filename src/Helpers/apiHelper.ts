54407e3d59233beb027564c2144851a1d8a0fb34
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-04-21 20:03:44 +0200

    fix: Ajout de la tsdoc manquante

--- src/Helpers/apiHelper.ts
+++ src/Helpers/apiHelper.ts
@@ -226,6 +226,23 @@ export const postElementByEndpoint = async (endpoint: string, data: {
     });
 };
 
+/**
+ * Envoie une requête DELETE à une API pour supprimer un titre spécifié.
+ * Cette fonction fait une requête DELETE à l'endpoint spécifié, en utilisant un token JWT pour l'authentification
+ * et inclut les détails du titre à supprimer dans le corps de la requête.
+ *
+ * @param endpoint - L'endpoint à appeler pour la suppression, qui sera concaténé avec la base URL de l'API.
+ * @param data - Un objet contenant le token d'authentification et le titre à supprimer.
+ *               Le token est utilisé pour authentifier la requête.
+ *               Le titre est l'objet à envoyer dans le corps de la requête pour spécifier quel titre doit être supprimé.
+ * @returns Une promesse qui se résout avec la réponse HTTP obtenue suite à la requête DELETE.
+ *
+ * @example
+ * deleteTitle('titles/delete', {
+ *   token: 'jwt-token-here',
+ *   title: { id: 'title-id', label: 'Example Title', value: 'example-value' }
+ * });
+ */
 export const deleteTitle = async (endpoint?: string, data?: {
     token: string;
     title?: Titles;
@@ -244,6 +261,23 @@ export const deleteTitle = async (endpoint?: string, data?: {
     });
 };
 
+/**
+ * Envoie une requête DELETE à une API pour supprimer un utilisateur spécifié.
+ * Cette fonction effectue une requête DELETE à l'endpoint spécifié, utilisant un token JWT pour l'authentification
+ * et inclut les détails de l'utilisateur à supprimer dans le corps de la requête.
+ *
+ * @param endpoint - L'endpoint à appeler pour la suppression, qui sera concaténé avec la base URL de l'API.
+ * @param data - Un objet contenant le token d'authentification et l'utilisateur à supprimer.
+ *               Le token est utilisé pour authentifier la requête.
+ *               L'utilisateur est l'objet à envoyer dans le corps de la requête pour spécifier quel utilisateur doit être supprimé.
+ * @returns Une promesse qui se résout avec la réponse HTTP obtenue suite à la requête DELETE.
+ *
+ * @example
+ * deleteUser('users/delete', {
+ *   token: 'jwt-token-here',
+ *   user: { id: 'user-id', name: 'John Doe', email: 'john.doe@example.com' }
+ * });
+ */
 export const deleteUser = async (endpoint?: string, data?: {
     token: string;
     user: User;
@@ -262,6 +296,23 @@ export const deleteUser = async (endpoint?: string, data?: {
     });
 }
 
+/**
+ * Envoie une requête PATCH à une API pour réinitialiser les points d'un utilisateur spécifié.
+ * Cette fonction effectue une requête PATCH à l'endpoint spécifié, utilisant un token JWT pour l'authentification
+ * et inclut les détails de l'utilisateur pour lequel les points doivent être réinitialisés dans le corps de la requête.
+ *
+ * @param endpoint - L'endpoint à appeler pour la réinitialisation des points, qui sera concaténé avec la base URL de l'API.
+ * @param data - Un objet contenant le token d'authentification et l'utilisateur dont les points doivent être réinitialisés.
+ *               Le token est utilisé pour authentifier la requête.
+ *               L'utilisateur est l'objet à envoyer dans le corps de la requête pour spécifier pour quel utilisateur les points doivent être réinitialisés.
+ * @returns Une promesse qui se résout avec la réponse HTTP obtenue suite à la requête PATCH.
+ *
+ * @example
+ * resetPointsUser('users/reset-points', {
+ *   token: 'jwt-token-here',
+ *   user: { id: 'user-id', name: 'John Doe', email: 'john.doe@example.com' }
+ * });
+ */
 export const resetPointsUser = async (endpoint?: string, data?: {
     token: string;
     user: User;
@@ -280,6 +331,23 @@ export const resetPointsUser = async (endpoint?: string, data?: {
     });
 }
 
+/**
+ * Envoie une requête POST à une API pour gérer une opération de paiement ou de commande spécifiée.
+ * Cette fonction réalise une requête POST vers l'endpoint spécifié, utilisant un token JWT pour l'authentification.
+ * Elle inclut les détails nécessaires (comme l'ID de l'API de paiement) dans le corps de la requête pour traiter l'opération.
+ *
+ * @param endpoint - L'endpoint de l'API auquel faire la requête POST, qui sera concaténé avec la base URL de l'API.
+ * @param data - Un objet optionnel contenant le token d'authentification et l'identifiant de l'API nécessaire à l'opération.
+ *               Le `token` est utilisé pour authentifier la requête auprès de l'API.
+ *               `idApi` est l'identifiant nécessaire pour la requête spécifique à l'opération de paiement ou commande.
+ * @returns Une promesse qui se résout avec la réponse HTTP obtenue suite à la requête POST.
+ *
+ * @example
+ * handleCheckout('checkout/initiate', {
+ *   token: 'jwt-token-here',
+ *   idApi: 'api-id-for-operation'
+ * });
+ */
 export const handleCheckout = async (endpoint: string, data?: {
     token: string;
     idApi: string;
