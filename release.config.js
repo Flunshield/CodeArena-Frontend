da8ab6758cbcce3e95bb8daddb73b1d14aa02c06
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-05-05 17:48:11 +0200

    fix(HOTFIX): correctif function findLastCommande

--- release.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    branches: [
-        "main",  // Assurez-vous que le nom de la branche est correct
-        { name: "beta", prerelease: true },
-        { name: "alpha", prerelease: true }
-    ],
-};
