609adca1180d0ee80ecf126c71f29e40db3a9aaf
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-05-05 12:57:28 +0200

    fix: fix cd

--- release.config.js
+++ release.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+// release.config.js converti en ES Module
+export default {
     branches: [
-        "main",
+        "main",  // Assurez-vous que le nom de la branche est correct
+        { name: "beta", prerelease: true },
+        { name: "alpha", prerelease: true }
     ],
 };
