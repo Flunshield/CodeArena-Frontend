db84b2669e167936eb1a9f77ec486f7379555543
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-05-06 08:33:28 +0200

    fix: delete fichier inutile

--- release.config.js
+++
@@ -1,8 +0,0 @@
-// release.config.js converti en ES Module
-export default {
-    branches: [
-        "main",  // Assurez-vous que le nom de la branche est correct
-        { name: "beta", prerelease: true },
-        { name: "alpha", prerelease: true }
-    ],
-};
