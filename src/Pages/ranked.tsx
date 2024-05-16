4c112bfab75dcb4a00e1b9cc7075c8b34d688d13
Kévin Begot <begot.kevin@gmail.com>
2024-05-16 17:11:42 +0200

    [ADD] loader and button for findMatch in ranked vue

--- src/Pages/ranked.tsx
+++ src/Pages/ranked.tsx
@@ -1,13 +1,50 @@
+import Button from '../ComposantsCommun/Button';
+import useLoader from '../ComposantsCommun/LoaderMatch.tsx';
 import Layout from "../ComposantsCommun/Layout.tsx";
+import { useAuthContext } from "../AuthContext.tsx";
+import { JwtPayload } from "jwt-decode";
+import { DataToken } from "../Interface/Interface.ts";
+import { postElementByEndpoint, getElementByEndpoint } from "../Helpers/apiHelper.ts";
+
+
 
 function Ranked() {
+    
+    const authContext = useAuthContext();
+    
+    const infosUser = authContext?.infosUser as JwtPayload
+    const infos = infosUser.aud as unknown as DataToken
+    
+    const FindMatch = async () => {
+        const id = infos.data.id ?? '';
+        console.log('id: ', id);
+        
+        const response = await postElementByEndpoint(`matchmaking/joinQueue`, {
+            token: authContext.accessToken ?? '',
+            data: {id}
+        })
+    
+        if (response.status === 201) {
+            console.log("ajouter à la file d'attente et recherhce de match en cours");
+            const response = await getElementByEndpoint('matchmaking/queue', {
+                token: authContext.accessToken ?? "",
+                data: ''
+            })
+            if (response.status === 201) {
+                console.log("Match trouvé");
+                alert("Match trouvé");
+            }
+        } else {
+            alert("Erreur lors de la recherche de match");
+        }
+    }
 
     return (
         <Layout>
-            <div className="m-32 h-full bg-secondary rounded-lg text-white">
-
-                salut
+            <div className="m-2 text-white flex justify-center">
+                <Button id="ranked-button" type={'button'} className="bg-green-500 m-2 p-2 rounded-md" onClick={FindMatch}>Trouver un match</Button>
             </div>
+            {useLoader()}
         </Layout>
     );
 }
