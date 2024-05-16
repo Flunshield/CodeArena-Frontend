6cf5e880986ecc3fe7fb29853896fbe20816b604
Kévin Begot <begot.kevin@gmail.com>
2024-05-16 17:09:37 +0200

    [ADD] matchLoader

---
+++ src/ComposantsCommun/LoaderMatch.tsx
@@ -0,0 +1,18 @@
+import '../css/Loader.css'; // Assurez-vous que le fichier CSS est correctement importé
+
+const useLoader = () => {
+    return (
+        <div>
+            <div className="loadingspinner">
+                <div id="square1"></div>
+                <div id="square2"></div>
+                <div id="square3"></div>
+                <div id="square4"></div>
+                <div id="square5"></div>
+            </div>
+            <span className='text-white flex justify-center font-bold text-xl'>Recherche d'un Match</span>
+        </div>
+    );
+}
+
+export default useLoader;
