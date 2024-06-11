0dbcae0368b93f1f3281ecf8fd88b7e7e85ea6ca
Kévin Begot <begot.kevin@gmail.com>
2024-06-11 18:18:09 +0200

    [ADD] matchmaking uuid && secure request

--- src/hook/useMatchmaking.ts
+++ src/hook/useMatchmaking.ts
@@ -22,14 +22,24 @@ const useMatchmaking = () => {
             return;
         }
 
-        const refreshQueueStatus = async () => {
+        const refreshStatus = async () => {
             setLoading(true);
             const isInQueue = await checkIsInQueue(authContext.accessToken ?? "", id);
             setInQueue(isInQueue);
+            console.log("isInQueue", isInQueue);
+            
+            const checkRoom = await checkIsInRoom(authContext.accessToken ?? "", id);
+            if (checkRoom.isInRoom) {
+                setMatchFound(true);
+                setRoomId(checkRoom.roomId);
+                console.log("isInRoom", checkRoom.isInRoom);
+                console.log("roomId", checkRoom.roomId);
+            }
+            
             setLoading(false);
         };
 
-        refreshQueueStatus();
+        refreshStatus();
     }, [authContext.accessToken, id]);
 
     const handleJoinQueue = useCallback(async () => {
@@ -101,26 +111,53 @@ export default useMatchmaking;
 
 async function checkIsInQueue(token: string, id: number) {
     try {
-        const response = await getElementByEndpoint('matchmaking/getQueue', {
+        const response = await getElementByEndpoint(`matchmaking/isInQueue?userId=` + id, {
             token,
             data: ''
         });
 
         if (response.status === 200) {
             const responseData = await response.json();
-            const queue = responseData.queue as number[];
-            if (!isNaN(id)) {
-                return queue.includes(id);
+            if (responseData.success) {
+                return responseData.isInQueue;
             } else {
-                console.error("L'ID utilisateur n'est pas valide");
+                console.error("Erreur lors de la vérification de la file d'attente : succès est faux");
                 return false;
             }
         } else {
-            console.log("Erreur lors de la file d'attente du match");
+            console.log("Erreur lors de la vérification de la file d'attente du match");
             return false;
         }
     } catch (error) {
         console.error("Erreur lors de la vérification de la file d'attente :", error);
         return false;
+    }
+}
+
+async function checkIsInRoom(token: string, id: number) {
+    try {
+        const response = await getElementByEndpoint(`matchmaking/isInRoom?userId=` + id, {
+            token,
+            data: ''
+        });
+
+        if (response.status === 200) {
+            const responseData = await response.json();
+            if (responseData.success) {
+                return {
+                    isInRoom: responseData.isInRoom,
+                    roomId: responseData.roomId
+                };
+            } else {
+                console.error("Erreur lors de la vérification de la salle de match : succès est faux");
+                return { isInRoom: responseData.isInRoom, roomId: null };
+            }
+        } else {
+            console.log("Erreur lors de la vérification de la salle de match");
+            return { isInRoom: false, roomId: null };
+        }
+    } catch (error) {
+        console.error("Erreur lors de la vérification de la salle de match :", error);
+        return { isInRoom: false, roomId: null };
     }
 }
