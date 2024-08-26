c1f014eb874060117589099479a8cf8b2e6e2f23
gregoire.ivars <gregoire.ivars@inetum.com>
2024-08-26 12:31:00 +0200

    feat: calendar et footer

--- src/Pages/tournamentDashboard.tsx
+++ src/Pages/tournamentDashboard.tsx
@@ -1,14 +1,11 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TableauTournament from "../Composants/tournament/TableauTournament.tsx";
 import Layout from "../ComposantsCommun/Layout.tsx";
 import { getElementByEndpoint } from "../Helpers/apiHelper.ts";
 import { useAuthContext } from "../AuthContext.tsx";
 import { Tournament } from "../Interface/Interface.ts";
 import { Container } from "../ComposantsCommun/Container.tsx";
-import { SectionIntro } from "../ComposantsCommun/SectionIntro.tsx";
-import Card from "../ComposantsCommun/Card.tsx";
-import CardContent from "../ComposantsCommun/CardContent.tsx";
-import { GridPattern } from "../ComposantsCommun/GridPattern.tsx";
+
 
 function TournamentDashboard() {
     const authContext = useAuthContext();
@@ -27,26 +24,11 @@ function TournamentDashboard() {
 
     return (
         <Layout>
-    
             <Container className="py-12">
-            <GridPattern
-                className="absolute inset-0 -z-10 h-full w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_50%,transparent_60%)]"
-                yOffset={-256}
-            />
-                <SectionIntro 
-                    title="Upcoming Tournaments" 
-                    subtitle="Stay updated with the latest tournaments happening soon."
-                    className="mb-8"
+                <TableauTournament
+                    infosTournament={infosTournament}
+                    className="border-0"
                 />
-                <Card className="bg-primary shadow-elevated p-6">
-                    <CardContent>
-                        <TableauTournament 
-                            infosTournament={infosTournament} 
-                            isImg={true} 
-                            className="border-0"
-                        />
-                    </CardContent>
-                </Card>
             </Container>
         </Layout>
     );
