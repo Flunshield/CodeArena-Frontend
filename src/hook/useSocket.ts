ac86a21b9f13cebb6eb4a313aa199a1302519ae7
Kévin Begot <begot.kevin@gmail.com>
2024-08-27 10:22:14 +0200

    [CHANGE] socket for dynamic puzzle

--- src/hook/useSocket.ts
+++ src/hook/useSocket.ts
@@ -1,24 +1,31 @@
 import { useEffect, useMemo } from 'react';
 import { io } from 'socket.io-client';
-import { MatchFoundEvent } from "../Interface/chatInterface";
+import { MatchFoundEvent, Puzzle } from "../Interface/chatInterface";
 
-const useSocket = (id: number, setMatchFound: (value: boolean) => void, setRoomId: (value: string | null) => void) => {
+const useSocket = (
+    id: number, 
+    setMatchFound: (value: boolean) => void, 
+    setRoomId: (value: string | null) => void,
+    setPuzzle: (value: Puzzle | null) => void
+  ) => {
     const socket = useMemo(() => io(import.meta.env.VITE_API_BASE_URL_BACK), []);
 
     useEffect(() => {
-        const handleMatchFound = ({ userId1, userId2, roomId }: MatchFoundEvent) => {
+        const handleMatchFound = ({ userId1, userId2, roomId, puzzle }: MatchFoundEvent) => {
             if (userId1 === id || userId2 === id) {
                 setMatchFound(true);
                 setRoomId(roomId);
+                setPuzzle({
+                    ...puzzle,
+                  });
             }
         };
 
-        socket.on('matchFound', handleMatchFound);
-
+        socket.on('matchFound', handleMatchFound);        
         return () => {
             socket.off('matchFound', handleMatchFound);
         };
-    }, [id, socket, setMatchFound, setRoomId]);
+    }, [id, socket, setMatchFound, setRoomId, setPuzzle]);
 
     return socket;
 };
