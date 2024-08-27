1b5ecea8ab591162d487512a4b1eafbd0b3df14e
Kévin Begot <begot.kevin@gmail.com>
2024-08-27 10:21:13 +0200

    [ADD] ide and puzzle display

--- src/Pages/ranked.tsx
+++ src/Pages/ranked.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import Layout from "../ComposantsCommun/Layout";
+import AceEditor from "react-ace";
 import Button from '../ComposantsCommun/Button';
 import LoaderMatch from '../ComposantsCommun/LoaderMatch';
 import Chat from "../Composants/Chat/Chat";
@@ -15,15 +16,17 @@ const Ranked = () => {
         roomId,
         id,
         username,
+        puzzle,
         handleJoinQueue,
         handleLeaveQueue,
         handleLeaveRoom,
         setMatchFound,
-        setRoomId
+        setRoomId,
+        setPuzzle
     } = useMatchmaking();
 
     if (id !== undefined) {
-        useSocket(id, setMatchFound, setRoomId);
+        useSocket(id, setMatchFound, setRoomId, setPuzzle);
     }
 
     const handleJoinQueueClick = () => {
@@ -37,9 +40,39 @@ const Ranked = () => {
     return (
         <Layout>
             {matchFound && roomId && id && username && (
-                <Chat roomId={roomId} userId={id} username={username} />
+                <div>
+                    <div className='text-center bg'>
+                        <h1>{puzzle?.title }</h1>
+                        <p>{puzzle?.details}</p>
+                    </div>
+                    <div className='flex'>
+                        <div className='w-full'>
+                            <AceEditor
+                                mode="javascript"
+                                name="AceEditor"
+                                editorProps={{ $blockScrolling: true }}
+                                style={{ height: '500px', width: '100%' }}
+                                className="border-2 border-white rounded-md"
+                            />
+                            <div className='flex justify-end p-2'>
+                                <Button
+                                    id="validate-button"
+                                    type="button"
+                                    className="inline-flex items-center px-4 py-2 bg-green-600 transition ease-in-out delay-75 hover:bg-green-700 text-tertiari text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110 gap-1"
+
+                                >
+                                    {'Valider'}
+                                    <img src="/assets/arrowRightWhite.svg" className="w-5 text-tertiari" alt="rejoindre" />
+                                </Button>
+                            </div>
+
+                        </div>
+
+                        <Chat roomId={roomId} userId={id} username={username} />
+                    </div>
+                </div>
             )}
-            <div className="m-2 text-tertiari flex flex-col items-center py-[120px]">
+            <div className="m-2 text-tertiari flex flex-col items-center pb-[120px]">
                 <div className='mb-4'>
                     {(loading || (inQueue && !matchFound)) && <LoaderMatch msg={t(`Recherche d'un match en cours...`)} />}
                 </div>
