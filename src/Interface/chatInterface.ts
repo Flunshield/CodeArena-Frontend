ebf3bcc1dc5bb2f46429a33b07300f4417283142
Kévin Begot <begot.kevin@gmail.com>
2024-08-27 10:20:43 +0200

    [CHANGE] puzzle Interface

--- src/Interface/chatInterface.ts
+++ src/Interface/chatInterface.ts
@@ -16,4 +16,19 @@ export interface MatchFoundEvent {
     userId1: number;
     userId2: number;
     roomId: string;
+    puzzle: Puzzle;
+}
+
+export interface Puzzle {
+    id: number;
+    rankingsID: number;
+    tournamentID: number | null;
+    eventsID: number | null;
+    tests: {
+      success: boolean;
+      testPassed: string[];
+      testFailed: string[];
+    };
+    details: string;
+    title: string;
 }
