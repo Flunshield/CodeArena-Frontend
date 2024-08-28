0065fbd88f5f1e612bc843ae73bbcbb835d1eaf5
gregoire.ivars <gregoire.ivars@inetum.com>
2024-08-28 09:29:44 +0200

    fix: reajustement de sectionIntro

--- src/ComposantsCommun/SectionIntro.tsx
+++ src/ComposantsCommun/SectionIntro.tsx
@@ -1,16 +1,21 @@
 import clsx from 'clsx'
 
-import {Container} from './Container'
-import {FadeIn} from './FadeIn'
+import { Container } from './Container'
+import { FadeIn } from './FadeIn'
 
 export function SectionIntro({
-                                 children,
-                                 invert = false,
-                                 ...props
-                             }: Omit<
+    title,
+    eyebrow,
+    children,
+    smaller = false,
+    invert = false,
+    ...props
+
+}: Omit<
     React.ComponentPropsWithoutRef<typeof Container>,
     'title' | 'children'
 > & {
+    title: string
     eyebrow?: string
     children?: React.ReactNode
     smaller?: boolean
@@ -18,7 +23,33 @@ export function SectionIntro({
 }) {
     return (
         <Container {...props}>
-            <FadeIn className="">
+            <FadeIn >
+            <h2>
+                    {eyebrow && (
+                        <>
+                            <span
+                                className={clsx(
+                                    'mb-6 block font-display text-base font-semibold',
+                                    invert ? 'text-tertiari' : 'text-neutral-950',
+                                )}
+                            >
+                                {eyebrow}
+                            </span>
+                            <span className="sr-only"> - </span>
+                        </>
+                    )}
+                    <span
+                        className={clsx(
+                            'block font-display tracking-tight [text-wrap:balance]',
+                            smaller
+                                ? 'text-2xl font-semibold'
+                                : 'text-2xl font-medium sm:text-3xl',
+                            invert ? 'text-tertiari' : 'text-neutral-950',
+                        )}
+                    >
+                        {title}
+                    </span>
+                </h2>
                 {children && (
                     <div
                         className={clsx(
@@ -33,3 +64,5 @@ export function SectionIntro({
         </Container>
     )
 }
+
+
