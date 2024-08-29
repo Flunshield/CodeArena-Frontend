b5698cc7d75b74771b94d90cb614791817db3319
gregoire.ivars <gregoire.ivars@inetum.com>
2024-08-29 11:11:33 +0200

    feat: mise à jour affichage page eventDashboard

--- src/Composants/event/AllEvent.tsx
+++ src/Composants/event/AllEvent.tsx
@@ -7,64 +7,71 @@ import { useTranslation } from "react-i18next";
 import { formatDate } from "../../Helpers/formatHelper.ts";
 import { FadeIn, FadeInStagger } from "../../ComposantsCommun/FadeIn.tsx";
 import { Container } from "../../ComposantsCommun/Container.tsx";
+import { useNavigate } from "react-router-dom";  // Import useNavigate
 
 interface TableauEventProps {
-    infosEvents: Event[];
-    isImg: boolean;
-    className?: string;
+  infosEvents: Event[];
+  isImg: boolean;
+  className?: string;
 }
 
 function AllEvent({ infosEvents, className }: TableauEventProps) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const navigate = useNavigate();  // Initialize useNavigate
 
-    return (
-        <Container className="px-4 sm:px-6 md:px-8 lg:px-12">
-        <Card className={`rounded-xl shadow-lg ${className}`}>
-          <CardContent className="bg-secondary text-tertiari text-center pb-6">
-            <p className="font-bold text-lg sm:text-2xl lg:text-4xl" id="title-event">
-              {t("event")}
-            </p>
-          </CardContent>
-          {infosEvents && infosEvents.length > 0 ? (
-            <FadeInStagger
-              className={`mt-6 sm:mt-8 lg:mt-10 grid gap-6 ${
-                infosEvents.length === 1
-                  ? "grid-cols-1"
-                  : infosEvents.length === 2
+  // Function to handle redirection to event details
+  const handleEventClick = (eventId: number) => {
+    navigate(`/event/${eventId}`);
+  };
+
+  return (
+    <Container className="px-4 sm:px-6 md:px-8 lg:px-12">
+      <Card className={`rounded-xl shadow-lg ${className}`}>
+        <CardContent className="bg-secondary text-tertiari text-center pb-6">
+          <p className="font-bold text-lg sm:text-2xl lg:text-4xl" id="title-event">
+            {t("event")}
+          </p>
+        </CardContent>
+        {infosEvents && infosEvents.length > 0 ? (
+          <FadeInStagger
+            className={`mt-6 sm:mt-8 lg:mt-10 grid gap-6 ${infosEvents.length === 1
+                ? "grid-cols-1"
+                : infosEvents.length === 2
                   ? "grid-cols-1 sm:grid-cols-2"
                   : "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
               }`}
-            >
-              {infosEvents.map((item: Event, index: number) => (
-                <FadeIn key={index} className="flex flex-col items-center">
-                  <Card className="bg-tertiari rounded-xl m-3 sm:m-4 lg:m-5 border-tertiary shadow-md w-full">
-                    <CardContent className="text-secondary p-4 sm:p-5">
-                      <div className="flex flex-col items-center">
-                        <p className="font-bold text-lg sm:text-xl lg:text-2xl mb-2 sm:mb-4 lg:mb-6">
-                          {item.title}
-                        </p>
-                        <p className="mb-2 sm:mb-3 lg:mb-4">{formatDate(item.startDate, t)}</p>
-                        <p className="text-secondary text-justify text-sm sm:text-base lg:text-lg">
-                          {item.description}
-                        </p>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </FadeIn>
-              ))}
-            </FadeInStagger>
-          ) : (
-            <CardContent className="bg-secondary text-tertiari border-2 rounded-lg m-4 sm:m-6 md:m-8 lg:m-10 flex justify-center items-center">
-              <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 sm:mb-6 md:mb-8 lg:mb-10 mt-4 sm:mt-6 md:mt-8 lg:mt-10">
-                {t("noEvent")}
-              </p>
-            </CardContent>
-          )}
-        </Card>
-      </Container>
-      
-      
-    );
+          >
+            {infosEvents.map((item: Event, index: number) => (
+              <FadeIn key={index} className="flex flex-col items-center">
+                <Card
+                  className="bg-tertiari rounded-xl m-3 sm:m-4 lg:m-5 border-tertiary shadow-md w-full cursor-pointer 
+                                    hover:shadow-xl hover:scale-105 transition-transform duration-300"
+                >
+                  <CardContent className="text-secondary p-4 sm:p-5">
+                    <div className="flex flex-col items-center" onClick={() => item.id && handleEventClick(item.id)}>
+                      <p className="font-bold text-lg sm:text-xl lg:text-2xl mb-2 sm:mb-4 lg:mb-6">
+                        {item.title}
+                      </p>
+                      <p className="mb-2 sm:mb-3 lg:mb-4">{formatDate(item.startDate, t)}</p>
+                      <p className="text-secondary text-justify text-sm sm:text-base lg:text-lg">
+                        {item.description}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </FadeIn>
+            ))}
+          </FadeInStagger>
+        ) : (
+          <CardContent className="bg-secondary text-tertiari border-2 rounded-lg m-4 sm:m-6 md:m-8 lg:m-10 flex justify-center items-center">
+            <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 sm:mb-6 md:mb-8 lg:mb-10 mt-4 sm:mt-6 md:mt-8 lg:mt-10">
+              {t("noEvent")}
+            </p>
+          </CardContent>
+        )}
+      </Card>
+    </Container>
+  );
 }
 
 export default AllEvent;
