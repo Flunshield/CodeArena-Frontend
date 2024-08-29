6c9759566f64cab6e3cef499ff7df4bc2db24b50
Kévin Begot <begot.kevin@gmail.com>
2024-08-29 13:46:08 +0200

    feat: Add code editor and submission functionality to Ranked page

--- src/Pages/ranked.tsx
+++ src/Pages/ranked.tsx
@@ -7,6 +7,8 @@ import Chat from "../Composants/Chat/Chat";
 import useMatchmaking from '../hook/useMatchmaking';
 import useSocket from '../hook/useSocket';
 import CountdownTimer from './countDownTimer';
+import { useState } from 'react';
+
 
 const Ranked = () => {
     const { t } = useTranslation();
@@ -19,18 +21,23 @@ const Ranked = () => {
         username,
         puzzle,
         startTimestamp,
+        testCallback,
         handleJoinQueue,
         handleLeaveQueue,
         handleLeaveRoom,
         handleEndMatch,
+        handleSubmitCode,
         setMatchFound,
         setRoomId,
         setPuzzle,
+        setInQueue,
         setStartTimestamp,
     } = useMatchmaking();
 
+    const [code, setCode] = useState("");
+
     if (id !== undefined) {
-        useSocket(id, setMatchFound, setRoomId, setPuzzle, setStartTimestamp);
+        useSocket(id, setMatchFound, setRoomId, setPuzzle, setStartTimestamp, setInQueue);
     }
 
     const handleJoinQueueClick = () => {
@@ -58,11 +65,13 @@ const Ranked = () => {
                                 editorProps={{ $blockScrolling: true }}
                                 style={{ height: '500px', width: '100%' }}
                                 className="border-2 border-white rounded-md"
+                                value={code} // Lier l'état 'code' à l'éditeur
+                                onChange={(newCode) => setCode(newCode)} // Mettre à jour l'état quand l'utilisateur tape
                             />
                             <div className='flex justify-between py-2'>
-                                <CountdownTimer 
-                                    key={startTimestamp} 
-                                    startTimestamp={startTimestamp || 0} 
+                                <CountdownTimer
+                                    key={startTimestamp}
+                                    startTimestamp={startTimestamp || 0}
                                     onComplete={handleEndMatch}
                                 />
 
@@ -70,6 +79,7 @@ const Ranked = () => {
                                     id="validate-button"
                                     type="button"
                                     className="inline-flex items-center px-4 py-2 bg-green-600 transition ease-in-out delay-75 hover:bg-green-700 text-tertiari text-sm font-medium rounded-md hover:-translate-y-1 hover:scale-110 gap-1"
+                                    onClick={() => handleSubmitCode(code)}
                                 >
                                     {'Valider'}
                                     <img src="/assets/arrowRightWhite.svg" className="w-5 text-tertiari" alt="rejoindre" />
@@ -80,6 +90,32 @@ const Ranked = () => {
 
                         <Chat roomId={roomId} userId={id} username={username} />
                     </div>
+                    {testCallback && (
+                        <div className='mt-4 p-4 bg-tertiari rounded-lg'>
+                            <p className='text-lg font-semibold text-gray-800 mb-3'>{testCallback.message}</p>
+                            {testCallback.testPassed.length > 0 && (
+                                <div className='mb-3'>
+                                    <h4 className='text-green-700 font-medium'>Tests valides :</h4>
+                                    <ul className='list-disc list-inside text-green-600'>
+                                        {testCallback.testPassed.map((test, index) => (
+                                            <li key={index}>{test}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                            {testCallback.testFailed.length > 0 && (
+                                <div>
+                                    <h4 className='text-red-700 font-medium'>Tests ratés :</h4>
+                                    <ul className='list-disc list-inside text-red-600'>
+                                        {testCallback.testFailed.map((test, index) => (
+                                            <li key={index}>{test}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    )}
+
                 </div>
             )}
             <div className="m-2 text-tertiari flex flex-col items-center pb-[120px]">
